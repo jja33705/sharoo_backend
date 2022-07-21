d4dacdb136c0fb009f45fb0b81442b790fef0334
jangjaehyeon <jja33705@gmail.com>
2022-07-21 17:43:57 +0900

    주석

--- middlewares/HousePermissionCheck.js
+++ middlewares/HousePermissionCheck.js
@@ -1,5 +1,6 @@
 const { House } = require('../models');
 
+// 하우스를 수정하거나 삭제할 권한이 있는지 체크하는 미들웨어
 const housePermissionCheck = async (req, res, next) => {
     const house = await House.findByPk(req.params.id);
 
