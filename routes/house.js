2ed84ce054d3f8f2f32832721b1c26e42ea7f85c
jangjaehyeon <jja33705@gmail.com>
2022-07-21 17:45:36 +0900

    테스트

--- routes/house.js
+++ routes/house.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const router = express.Router();
 const jwtAuthentication = require('../middlewares/jwtAuthentication');
-const housePermissionCheck = require('../middlewares/housePermissionCheck');
+const housePermissionCheck = require('../middlewares/housePermission');
 const { uploadHouseImage } = require('../middlewares/upload');
 const { House, Image, User } = require('../models');
 
