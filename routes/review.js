e22ac9962d34777f41d28a85e530b80b05401240
jangjaehyeon <jja33705@gmail.com>
2022-07-22 11:32:29 +0900

    리뷰 라우터 세팅

--- routes/review.js
+++ routes/review.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const jwtAuthentication = require('../middlewares/jwtAuthentication');
 const router = express.Router();
+
+router.get('/:id', async (req, res) => {
+
+});
 
 module.exports = router;
